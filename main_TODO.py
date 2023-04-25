d44a389abe332fb9274abee0f8f799b71fe98e7a
sail <sxj749@case.edu>
2023-04-25 09:32:00 -0400

    new day

--- main_TODO.py
+++ main_TODO.py
@@ -32,10 +32,15 @@ class Player(pygame.sprite.Sprite):
             print(f"Distance to bh: {r} , Direction:{v_bh}")
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, img, pos_x, pos_y, speed_x, speed_y):
+    def __init__(self, pos_x, pos_y, speed_x, speed_y):
         super().__init__()
-        self.image = pygame.image.load(img)
-        self.image = pygame.transform.scale(self.image, SIZE)
+        self.sprites = []
+        img_0 = pygame.transform.scale(pygame.image.load('art/UfoGrey.png'), (32, 32))
+        img_1 = pygame.transform.scale(pygame.image.load('art/UfoGrey1.png'), (32, 32))
+        self.sprites.append(img_0)
+        self.sprites.append(img_1)
+        self.curr_index = 0
+        self.image = self.sprites[self.curr_index]
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
         self.speed_x = speed_x
@@ -43,15 +48,18 @@ class Enemy(pygame.sprite.Sprite):
 
 
     def update(self) -> None:
+        self.curr_index += 0.05
+        if self.curr_index > len(self.sprites) - 1:
+            self.curr_index = 0
+        self.image = self.sprites[round(self.curr_index)] 
         collision_forgive = 10
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
-        # limited inside screen
+        #TODO limited inside screen
         if self.rect.right >= screen_w or self.rect.left <= 1:
             self.speed_x *= -1
         if self.rect.bottom >= screen_h or self.rect.top <= 1:
             self.speed_y *= -1
-        #TODO force
         if pygame.sprite.spritecollide(player, enemy_group, False):    
             if abs(self.rect.right - player.rect.left) < collision_forgive and self.speed_x > 0\
             or abs(self.rect.left - player.rect.right) < collision_forgive and self.speed_x < 0:
@@ -80,8 +88,8 @@ class Blackhole(pygame.sprite.Sprite):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.sprites = []
-        img_0 = pygame.transform.scale(pygame.image.load('art/Hurricane.png'), (64, 64))
-        img_1 = pygame.transform.scale(pygame.image.load('art/Hurricane_1.png'), (64, 64))
+        img_0 = pygame.transform.scale(pygame.image.load('art/Hurricane.png'), (128, 128))
+        img_1 = pygame.transform.scale(pygame.image.load('art/Hurricane_1.png'), (128, 128))
         self.sprites.append(img_0)
         self.sprites.append(img_1)
         self.curr_index = 0
@@ -97,14 +105,35 @@ class Blackhole(pygame.sprite.Sprite):
         self.image = self.sprites[round(self.curr_index)] 
 
 
+class Earth(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.sprites = []
+        img_0 = pygame.transform.scale(pygame.image.load('art/Earth.png'), (128, 128))
+        # img_1 = pygame.transform.scale(pygame.image.load('art/Hurricane_1.png'), (128, 128))
+        self.sprites.append(img_0)
+        # self.sprites.append(img_1)
+        self.curr_index = 0
+        self.image = self.sprites[self.curr_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [self.pos_x, self.pos_y]
+
+
+    def update(self):
+        pass
+
 player_src = 'art/RocketWhite.png'
-enemy_src = 'art/UfoBlue.png' 
+# enemy_src = 'art/UfoBlue.png' 
 bullet_src = 'art/bullet.png'   
 SIZE = (32, 32)
 
 pygame.init()
 clock = pygame.time.Clock()
-
+#background music
+pygame.mixer.music.load('art/Battle in the winter.mp3')
+pygame.mixer_music.play()
 screen_w = 1024
 screen_h = 1024
 screen = pygame.display.set_mode((screen_w, screen_h))
@@ -121,42 +150,53 @@ player_group.add(player)
 
 #spawn enemy
 enemy_group = pygame.sprite.Group()
-for i in range(5):
-    enemy = Enemy(enemy_src, random.randrange(0, screen_w), random.randrange(0, screen_h),
+for i in range(15):
+    enemy = Enemy(random.randrange(0, screen_w), random.randrange(0, screen_h),
                   0.5* random.randrange(-5, 5), 0.5* random.randrange(-5, 5))
     enemy_group.add(enemy)
 #spawn bullet
 bullet_group = pygame.sprite.Group()
 #spawn blackhole
-blackhole = Blackhole(random.randrange(0, screen_w), random.randrange(0, screen_h))
+blackhole = Blackhole(200, 200)
 blackhole_group = pygame.sprite.Group()
 blackhole_group.add(blackhole)
+earth = Earth(800, 800)
+earth_group = pygame.sprite.Group()
+earth_group.add(earth)
 #Thread for serial
-t = threading.Thread(target=arduino_read, args=(pos_ar,), daemon=True)
-t.start()
+def main():
+    global bullet
+    t = threading.Thread(target=arduino_read, args=(pos_ar,), daemon=True)
+    t.start()
 
-#main loop
-while True:
-    # Uncomment this for position control
-    player.pos_x = pos_ar[0]+100
-    player.pos_y = pos_ar[1]+100
-    # print(f'x: {pos_ar[0]}, y:{pos_ar[1]}')
-    for even in pygame.event.get():
-        if even.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if even.type == pygame.MOUSEBUTTONDOWN:
-            bullet = player.fire()
-            bullet_group.add(bullet)
-    
-    screen.blit(background, (0,0))
-    blackhole_group.draw(screen)
-    player_group.draw(screen)
-    enemy_group.draw(screen)
-    bullet_group.draw(screen)
-    blackhole_group.update()
-    player_group.update()
-    enemy_group.update()
-    bullet_group.update()
-    pygame.display.flip()
-    clock.tick(60)
+    #main loop
+    while True:
+        # Uncomment this for position control
+        player.pos_x = pos_ar[0]+100
+        player.pos_y = pos_ar[1]+100
+        # print(f'x: {pos_ar[0]}, y:{pos_ar[1]}')
+        for even in pygame.event.get():
+            if even.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if even.type == pygame.MOUSEBUTTONDOWN:
+                bullet = player.fire()
+                bullet_group.add(bullet)
+        
+        screen.blit(background, (0,0))
+        blackhole_group.draw(screen)
+        earth_group.draw(screen)
+        player_group.draw(screen)
+        enemy_group.draw(screen)
+        bullet_group.draw(screen)
+        blackhole_group.update()
+        earth_group.update()
+        player_group.update()
+        enemy_group.update()
+        bullet_group.update()
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
