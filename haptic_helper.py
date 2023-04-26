b29f23142766fa438e335ef29dd724e1c7913b18
sail <sxj749@case.edu>
2023-04-25 22:29:26 -0400

    add trigger, todo write

--- haptic_helper.py
+++ haptic_helper.py
@@ -1,30 +1,36 @@
 import serial
 import time
 import math
-
-def arduino_read(data_from_arduino):
-    
-    arduino = serial.Serial('COM4', 115200, timeout=.1)
-
-    try:
-        arduino.open()
-        if arduino.is_open:
-            print('CONNECTED')
-    except:
-        pass
-    time.sleep(1)
-    while True:
-        arduino.readline = lambda: arduino.read_until(b'\n').rstrip(b'\n') # get rid of \n in serial
-        raw = arduino.readline()
+class HapkitCommute:
+    def __init__(self, com, baud):
+        self.arduino = serial.Serial(com, baud, timeout=.1)
+    def arduino_read(self, data_from_arduino):
+        
+        # arduino = serial.Serial('COM5', 115200, timeout=.1)
 
         try:
-            pos = raw.decode('utf-8').split(",")
-            data_from_arduino[0] = float(pos[0])
-            data_from_arduino[1] = float(pos[1])
-            arduino.flush()
-            # print(x_ar, y_ar)
+            self.arduino.open()
+            if self.arduino.is_open:
+                print('CONNECTED')
         except:
             pass
+        time.sleep(1)
+        while True:
+            self.arduino.readline = lambda: self.arduino.read_until(b'\n').rstrip(b'\n') # get rid of \n in serial
+            raw = self.arduino.readline()
+
+            try:
+                pos = raw.decode('utf-8').split(",")
+                data_from_arduino[0] = float(pos[0])
+                data_from_arduino[1] = float(pos[1])
+                data_from_arduino[2] = int(pos[2])
+                self.arduino.flush()
+                # print(x_ar, y_ar)
+            except:
+                pass
+    def arduino_write(self, force_x, force_y, damp = 0, ):
+                
+        pass
 def normalize_vector(vector):
     x, y = vector[0], vector[1]
     magnitude = math.sqrt(x**2 + y**2)
